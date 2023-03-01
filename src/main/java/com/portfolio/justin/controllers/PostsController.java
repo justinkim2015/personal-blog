ba23995bd5658472d7e08ff36e75047b4e7a05e3
Justin Kim <justinkim2015@gmail.com>
2023-03-01 10:35:57 +0900

    Create edit action

--- src/main/java/com/portfolio/justin/controllers/PostsController.java
+++ src/main/java/com/portfolio/justin/controllers/PostsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class PostsController {
     }
     
     @GetMapping("/posts/{id}")
-    Post getPost(@PathVariable Long id) {
+    public Post getPost(@PathVariable Long id) {
       
         return postRepository.findById(id)
         	      .orElseThrow(() -> new PostNotFoundException(id));
@@ -47,4 +48,19 @@ public class PostsController {
     void deletePost(@PathVariable Long id) {
     	postRepository.deleteById(id);
     }
+    
+    @PutMapping("posts/{id}")
+    public Post updatePost(@RequestBody Post newPost, @PathVariable Long id) {
+        
+        return postRepository.findById(id)
+          .map(post -> {
+            post.setTitle(newPost.getTitle());
+            post.setBody(newPost.getBody());
+            return postRepository.save(post);
+          })
+          .orElseGet(() -> {
+            newPost.setId(id);
+            return postRepository.save(newPost);
+          });
+      }
   }
