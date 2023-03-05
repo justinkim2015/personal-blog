8c8b6a609e577193d1dd8fed10dd5162e4173c3c
Justin Kim <justinkim2015@gmail.com>
2023-03-05 12:33:47 +0900

    Add basic parts of form

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,9 +1,32 @@
+import { TextField, Button } from "@mui/material";
+import { Container } from "@mui/system";
+
 const Login = () => {
   return (
-    <div>
-      Login here!
-    </div>
-  )
-}
+    <Container sx={{ mt: 4, width: 3 / 4 }}>
+      <form>
+        <TextField
+          required
+          id="username"
+          label="Username"
+          variant="filled"
+          sx={{ my: 1 }}
+        ></TextField>
+        <TextField
+          required
+          id="password"
+          type="password"
+          label="Password"
+          variant="filled"
+          sx={{ my: 1 }}
+        ></TextField>
 
-export default Login
+        <Button variant="contained" sx={{ mt: 1 }}>
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
