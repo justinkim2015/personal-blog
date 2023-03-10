c0c1f0ad29ffc178973aa68e2517a3f66eee15c5
Justin Kim <justinkim2015@gmail.com>
2023-03-11 08:51:16 +0900

    Add API request to login form

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,7 +1,42 @@
 import { TextField, Button } from "@mui/material";
 import { Container } from "@mui/system";
+import { useState } from "react";
 
 const Login = () => {
+  const initialFormState = {
+    username: "",
+    password: "",
+  };
+
+  const [login, setLogin] = useState(initialFormState);
+
+  const handleSubmit = async () => {
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(login),
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.error("Error logging in", error);
+    }
+  };
+
+  const handleChange = () => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    setLogin({
+      username: username,
+      password: password,
+    });
+  };
+
+
   return (
     <Container sx={{ mt: 4, width: 3 / 4 }}>
       <form>
