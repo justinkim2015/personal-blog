5e6a1633e4d824f29b4adb58e6503729bed5999a
Justin Kim <justinkim2015@gmail.com>
2023-03-13 15:56:04 +0900

    Set up full auth for all users

--- src/main/java/com/portfolio/justin/security/services/UserDetailsImpl.java
+++ src/main/java/com/portfolio/justin/security/services/UserDetailsImpl.java
@@ -1,10 +1,10 @@
 package com.portfolio.justin.security.services;
 
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 
 import com.portfolio.justin.models.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -16,24 +16,26 @@ public class UserDetailsImpl implements UserDetails {
 
     private String username;
 
-    private String email;
-
     @JsonIgnore
     private String password;
 
     private Collection<? extends GrantedAuthority> authorities;
 
-    public UserDetailsImpl(Long id, String username, String password) {
+    public UserDetailsImpl(Long id, String username, String password, List<GrantedAuthority> authorities) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.authorities = authorities != null ? authorities : Collections.singletonList(new SimpleGrantedAuthority("FULL_AUTHORITY"));
     }
 
     public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("FULL_AUTHORITY"));
         return new UserDetailsImpl(
                 user.getId(),
                 user.getUsername(),
-                user.getPassword()
+                user.getPassword(),
+                authorities
         );
     }
 
@@ -44,10 +46,6 @@ public class UserDetailsImpl implements UserDetails {
 
     public Long getId() {
         return id;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     @Override
